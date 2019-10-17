33204d10bd81c7fb1ba9c5827dbd35c425eddc17
Victor Anguenot <victor.anguenot@gmail.com>
2019-10-17 22:56:47 +0200

    17 octobre

--- src/Carte.cpp
+++ src/Carte.cpp
@@ -1,1 +1,29 @@
-#include"Carte.h"
+#include "Carte.h"
+#include<iostream>
+
+int Carte::compare(Carte &ct) {
+  if (this->valeur > ct.valeur)
+    return 1; // si la première carte est supérieure a la deuxième
+  else if (this->valeur < ct.valeur)
+    return 2; // si la première carte inférieure a la deuxième
+  else
+    return 0; // les deux cartes sont égales
+}
+void Carte::setvaleur(int val) { // set value
+  this->valeur = val;
+}
+void Carte::setcouleur(std::string coul) { // set color
+  this->couleur = coul;
+}
+int Carte::getvaleur() { // get valeur
+  return valeur;
+}
+std::string Carte::getcouleur() { // get couleur
+  return couleur;
+}
+void Carte::echange(Carte &ct) { // échanger deux cartes
+  Carte tmp = *this;             // carte temporaire
+  valeur = ct.valeur;
+  couleur = ct.couleur;
+  ct = tmp;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include"Carte.h"
 
 int main(){
     std::cout<<"aaa"<<std::endl;
+
+    Carte c;
+    c.setvaleur(2);
+    c.setcouleur("Coeur");
+
+    std::cout <<c.getcouleur()<< std::endl;
+    std::cout <<c.getvaleur()<< std::endl;
     return 0;
 }
